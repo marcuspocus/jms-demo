38e3d578158736f935cef1defa5a2acabe88308b
Marc Deschamps <marc.deschamps@flexitpro.com>
2011-06-02 15:30:26 +0200

    Initial push

---
+++ app/controllers/Application.java
@@ -0,0 +1,80 @@
+package controllers;
+
+import static play.libs.F.Matcher.ClassOf;
+import static play.libs.F.Matcher.Equals;
+import static play.mvc.Http.WebSocketEvent.SocketClosed;
+import static play.mvc.Http.WebSocketEvent.TextFrame;
+
+import javax.inject.Named;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+
+import play.Logger;
+import play.libs.F.Either;
+import play.libs.F.EventStream;
+import play.libs.F.Promise;
+import play.mvc.Controller;
+import play.mvc.Http.WebSocketClose;
+import play.mvc.Http.WebSocketEvent;
+import play.mvc.WebSocketController;
+
+public class Application extends Controller {
+
+
+	static final EventStream<String> stream = new EventStream<String>(100);
+
+	public static void index() {
+		render();
+	}
+
+	public void onMessage(final String msg) {
+		Logger.info("Message received in JMS...");
+		try {
+			stream.publish("Via JMS => " + msg);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static class WebSocket extends WebSocketController {
+
+		@Named("chat")
+		static JmsTemplate jms;
+
+		public static void echo() {
+
+			while (inbound.isOpen()) {
+				Either<WebSocketEvent, String> e = await(Promise.waitEither(inbound.nextEvent(), stream.nextEvent()));
+
+				for (String quit : TextFrame.and(Equals("quit")).match(e._1)) {
+					outbound.send("bye:%s", quit);
+					disconnect();
+				}
+
+				for (final String msg : TextFrame.match(e._1)) {
+					jms.send("chat", new MessageCreator() {
+						public Message createMessage(Session session) throws JMSException {
+							return session.createTextMessage(msg);
+						}
+					});
+				}
+
+				for (String msg : ClassOf(String.class).match(e._2)) {
+					outbound.send("%s", msg);
+				}
+
+				for (WebSocketClose closed : SocketClosed.match(e._1)) {
+					Logger.info("Socket Closed: %s", closed.toString());
+				}
+
+			}
+
+		}
+
+	}
+
+}
---
+++ app/jobs/Bootstrap.java
@@ -0,0 +1,36 @@
+package jobs;
+
+import javax.inject.Inject;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+
+import controllers.Application;
+
+import play.jobs.Job;
+import play.jobs.OnApplicationStart;
+
+@OnApplicationStart
+public class Bootstrap extends Job<Void> {
+	
+	@Inject
+	private static CamelContext camel;
+	
+	public void doJob(){
+		try {
+			RouteBuilder builder = new RouteBuilder() {
+				
+				@Override
+				public void configure() throws Exception {
+					from("activemq:queue:chat").id("chat").bean(Application.class, "onMessage");
+				}
+				
+			};
+			camel.addRoutes(builder);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
